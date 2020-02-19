c35cf207b25bcdd6de8d2990dff45f333613e320
zhezhu <979769265@qq.com>
2020-02-19 13:30:20 +0800

    2.20

---
+++ conf.py
@@ -0,0 +1,4 @@
+import multiprocessing
+
+
+bind = "127.0.0.1:8001"
--- video_project/urls.py
+++ video_project/urls.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from app.dashboard import urls as dashboard_urls
 from app.client import urls as client_urls
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('dashboard/', include(dashboard_urls)),
-    path('client/', include(client_urls)),
+    path('', include(client_urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+
+
+
+
